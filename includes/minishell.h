9ef2be6c1358af89acb1fcb60a538f43965c7860
Zachary Najda <zacharynajda@zacharys-air.home>
2022-07-18 23:07:54 +0200

    Adding some basic struct and start of enums for a token based system

--- includes/minishell.h
+++ includes/minishell.h
@@ -13,6 +13,22 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+/*This is defining all of our structs which will be used for minishell*/
+
+enum tokens {
+    Command_Name = 0,
+    Command_Arg = 1,
+    Break_PIPE = 2,
+    Break_
+};
+/*This contains all of our token type ids, and is a list for when they come
+this will store information so that way it is easier to parse later*/
+typedef struct s_token
+{
+    enum tokens token_id;
+    struct s_token *next;
+}               t_token;
+
 /*
 This is the core part of minishell, launching it from main.c
 it runs in an infinite loop reading an input from readline
