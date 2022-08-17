c774a0ba9123c1b5686dff30b79c5140969ea7de
Zachary Najda <znajda@f1r5s17.codam.nl>
2022-08-17 18:35:04 +0200

    Forgot to hand in one unsaved file

--- srcs/parser/parser_start.c
+++ srcs/parser/parser_start.c
@@ -46,15 +46,11 @@ t_parse *parser(char *input, char **env)
     head = parse_initalize();
 	if (!head)
 		return (NULL);
-	// input_temp = ft_strdup(input);
     temp = ft_split(input, ' ');
     if (!temp)
         return (head);
 	head->cmd = find_cmd_path(*temp, env);
 	head->args = temp_duplicate(temp + 1);
-	// printf("Command Name/Path:%s\n", head->cmd);
-	// for (int i = 0; head->args && head->args[i]; i++)
-	// 	printf("Command args [%d]:%s\n", i, head->args[i]);
 	free_my_lines(temp);
     return (head);
 }
