e95ffe3f0e182e6d75224b1361bcec4bfb163107
ydemura <109362182+ydemura@users.noreply.github.com>
2022-09-12 18:37:37 +0200

    Fd only child (#17)
    * adding files in apropriate plases in prjct
    
    * adjusted gitignore
    
    * gitign
    
    * project change
    
    Co-authored-by: Julia Demura <yuliia@Julias-MacBook-Air.local>

---
+++ includes/command_loop/fd_only_child.h
@@ -0,0 +1,14 @@
+//
+//  fd_only_child.h
+//  merged_mini_xcode_project
+//
+//  Created by Julia Demura on 12/09/2022.
+//  Copyright © 2022 Yuliia Demura. All rights reserved.
+//
+
+#ifndef fd_only_child_h
+#define fd_only_child_h
+
+#include <stdio.h>
+
+#endif /* fd_only_child_h */
---
+++ srcs/command_loop/fd_only_child.c
@@ -0,0 +1,9 @@
+//
+//  fd_only_child.c
+//  merged_mini_xcode_project
+//
+//  Created by Julia Demura on 12/09/2022.
+//  Copyright © 2022 Yuliia Demura. All rights reserved.
+//
+
+#include "fd_only_child.h"
