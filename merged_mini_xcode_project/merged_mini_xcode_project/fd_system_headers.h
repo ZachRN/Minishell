123b903e9a99b961b3f2549fab41575724ce90ab
Julia Demura <yuliia@Julias-MacBook-Air.local>
2022-09-12 15:46:39 +0200

    # Conflicts: #	merged_mini_xcode_project/merged_mini_xcode_project.xcodeproj/project.xcworkspace/xcuserdata/yuliia.xcuserdatad/UserInterfaceState.xcuserstate

---
+++ merged_mini_xcode_project/merged_mini_xcode_project/fd_system_headers.h
@@ -0,0 +1,18 @@
+//
+//  fd_system_headers.h
+//  merged_mini_xcode_project
+//
+//  Created by Julia Demura on 12/09/2022.
+//  Copyright © 2022 Yuliia Demura. All rights reserved.
+//
+
+#ifndef fd_system_headers_h
+#define fd_system_headers_h
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif /* fd_system_headers_h */
