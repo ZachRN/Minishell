4d5b6d72f59931d3cf459753eac03f0eaaa668fa
Zachary Najda <zacharynajda@zacharys-air.home>
2022-09-15 01:40:43 +0200

    I tried to fix some of the execution from what I could see what was wrong at home

--- srcs/command_loop/cmd_loop.c
+++ srcs/command_loop/cmd_loop.c
@@ -10,6 +10,7 @@
 #include "built_in_set.h"
 # include <unistd.h>
 #include <stdlib.h>
+#include <signal_handles.h>
 
 #include <stdio.h>
 
@@ -27,11 +28,11 @@ int	check_assess_to_file(const char *path)
 
 int	not_exeption_do_pipe(int i, int comm_n, t_type type)
 {
+	// printf("I is:%d\nComm_n is: %d", i, comm_n);
 	if (type == BUILTIN && comm_n == 1) ///first and builtin - I do not pipe
 		return (FLS);
-	else if (i > 0 && i == comm_n - 1)///last - I do not pipe
+	else if (i == comm_n - 1)///last - I do not pipe, got rid of i == 0
 		return (FLS);
-	
 	return (TRU);
 }
 
@@ -45,14 +46,18 @@ char **	handle_one_param_set(int i, int comm_number, char **envp, t_param *param
 		if (pipe(param->fd.pipe) < 0)
 			exit(1);
 	}
+	if (i == 0 && param->cmd.type == BUILTIN && comm_number == 1)
+	{
+		// printf("Shouldn't be here\n");
+		new_envp = enviromental_variable_function(envp, param->cmd.command, param->cmd.cmd_args);
+		return (new_envp);
+	}	
 	param->child_pid = fork();
 	if (param->child_pid < 0)
 		exit(1);
-	if (i == 0 && param->cmd.type == BUILTIN && comm_number == 1)
-		new_envp = enviromental_variable_function(envp, param->cmd.command, param->cmd.cmd_args);
-		
 	if (param->child_pid == 0) ///child was created, we enter its process
 	{
+		// printf("Should be here\n");
 		if (check_assess_to_file(param->path_infile) < 0) ///check for file access for infile and in case of failure skip to next command
 			exit(1);
 		///	maybe I also skip command if its builtin but not the first one?
@@ -68,7 +73,10 @@ char **	handle_one_param_set(int i, int comm_number, char **envp, t_param *param
 		}
 		exit(1);
 	}
+	while (wait(NULL) > 0)
+		continue;
 	manage_parent_fd(i, comm_number, &param->fd);
+	// printf("Seg fault prior new_envp return\n");
 	return (new_envp);
 }
 
@@ -80,21 +88,23 @@ int	go_through_commands(t_exec *exec)
 	while (exec->index < exec->comm_number)
 	{
 		exec->upd_envp = handle_one_param_set(i, exec->comm_number, exec->envp, exec[exec->index].params);
-		if (exec->upd_envp != NULL)
-		{
-			free_array_of_str(exec->envp);
-			exec->envp = exec->upd_envp;
-			exec->upd_envp = NULL;
-		}
+		// if (exec->upd_envp != NULL)
+		// {
+		// 	free_array_of_str(exec->envp);
+		// 	exec->envp = exec->upd_envp;
+		// 	exec->upd_envp = NULL;
+		// }
 		exec->index++;
 		i++;
 	}
-	return (exec[exec->index].params->child_pid);
+	// printf("Beofre the thingy\n");
+	// printf("This exec thing: %d", exec[exec->index].params->child_pid);
+	return (exec->params[exec->index].child_pid);
 }
 ///	at the end I expect to receive sequence of pod_t in every t_param
 ///
 
-void	loop_through_waitpid(int comm_num, t_param *params, int last_pid)
+int	loop_through_waitpid(int comm_num, t_param *params, int last_pid)
 {
 //	int i;
 //	int wait_status;
@@ -114,11 +124,28 @@ void	loop_through_waitpid(int comm_num, t_param *params, int last_pid)
 		int	p_id;
 		int ex;
 
+		ex = 0;
 		p_id = last_pid;
 		waitpid(p_id, &status, 0);
-
 		if (WIFEXITED(status))
 			ex = WEXITSTATUS(status);
+		while (wait(NULL) > 0)
+			continue;
+		return (ex);
+}
+
+void	free_exec_params(t_exec *exec)
+{
+	int i;
+
+	i = 0;
+
+	while (i < exec->comm_number)
+	{
+		free(exec->params[i].cmd.cmd_path);
+		i++;
+	}
+	free(exec->params);
 }
 
 t_exec	creat_exec_loop_commands(t_together *input, char **envp)
@@ -127,7 +154,12 @@ t_exec	creat_exec_loop_commands(t_together *input, char **envp)
 	int ch_pid;
 
 	exec = form_input_for_execution(envp, input);
+	// printf("Seg fault prior go through commands\n");
+	signal_director(PAUSE_SIG);
 	ch_pid = go_through_commands(&exec);
-	loop_through_waitpid(exec.comm_number, exec.params, ch_pid);
+	exec.last_error = loop_through_waitpid(exec.comm_number, exec.params, ch_pid);
+	signal_director(MAIN_SIG);
+	free_exec_params(&exec);
+	exec.params = NULL;
 	return (exec);
 }
