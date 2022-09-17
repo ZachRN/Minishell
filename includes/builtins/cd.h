13f8f2a4244dc2cf5628b3662a0e970fcda4e384
ydemura <109362182+ydemura@users.noreply.github.com>
2022-09-17 14:35:07 +0200

    Fixing cd (#34)
    * working on CD, found many problems in functions, on it. partially cd is working already
    
    * issue: in bash segfaults after make
    
    cd added getpwd for pwd in case of ".."
    
    * fixed cd, looks like working
    
    * added hotfix echo
    
    * hotfix echo 2
    
    Co-authored-by: Julia Demura <yuliia@Julias-MacBook-Air.local>

--- includes/builtins/cd.h
+++ includes/builtins/cd.h
@@ -10,6 +10,13 @@
 
 #include "builtin_utils.h"
 
+typedef struct	s_env_indexes
+{
+	int homevar;
+	int pwd;
+	int oldpwd;
+}				t_env_indexes;
+
 char**	cd_builtin(t_env_struct *data);
 
 #endif /* cd_h */
--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -9,23 +9,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int go_to_other_folder(const char *argument, const char *home_var, int fd)
+#include <stdio.h>
+
+int go_to_other_folder(const char *path, int fd)
 {
 	int res;
-	const char *path;
 
-	if (argument != NULL)
-		path = argument;
-	else
-	{
-		if (home_var != NULL)
-		{
-			while (*home_var != '=')
-				home_var++;
-			home_var++;
-			path = home_var;
-		}
-	}
 	res = chdir(path);
 	if (res < 0)
 	{
@@ -53,7 +42,7 @@ int	get_new_len(char *name, char *value)
 	return (len_str + len_2);
 }
 
-char *join_varname_value_malloc(char *name, char *value)
+char	*join_varname_value_malloc(char *name, char *value)
 {
 	char *str;
 	int i;
@@ -80,18 +69,25 @@ char *join_varname_value_malloc(char *name, char *value)
 	return (str);
 }
 
-int change_pwd_value(t_env_struct *data, int i_homevar, int i_pwd)
+int change_pwd_value(t_env_indexes i, t_env_struct *data) //const char *cd_arg
 {
-	if (data->new_envp[i_pwd] != NULL)
-		free(data->new_envp[i_pwd]);
-	if (data->arguments[0] == NULL && i_homevar > 0)
-		data->new_envp[i_pwd] = join_varname_value_malloc("PWD=", data->envp[i_homevar]);
+	char buff[1024];
+	
+	if (data->new_envp[i.pwd] != NULL)
+		free(data->new_envp[i.pwd]);
+	if (data->arguments == NULL && i.homevar > 0) //cd_arg == NULL
+		data->new_envp[i.pwd] = join_varname_value_malloc("PWD=", data->envp[i.homevar]);
 	else
-		data->new_envp[i_pwd] = ft_strjoin("PWD=", data->arguments[0]);
+	{
+		if (getcwd(buff, sizeof(buff)) == NULL)
+			exit(1);// not sure its correct
+		data->new_envp[i.pwd] = ft_strjoin("PWD=", buff);
+	}
+		
 	return (0);
 }
 
-int change_var_value_based_on_indexes(t_env_struct *data, int i_oldpwd, int i_pwd)
+int change_var_value_based_on_indexes(int i_oldpwd, int i_pwd, t_env_struct *data)
 {
 	if (data->new_envp[i_oldpwd] != NULL)
 		free(data->new_envp[i_oldpwd]);
@@ -101,60 +97,91 @@ int change_var_value_based_on_indexes(t_env_struct *data, int i_oldpwd, int i_pw
 	return (0);
 }
 
-char **	handle_enviromntl(t_env_struct *data, int i_homevar, int i_pwd, int i_oldpwd)
+char	**handle_enviromntl(t_env_indexes index, t_env_struct *data)
 {
-	int i;
+	int j;
 
-	i = 0;
-	while (data->envp[i] != NULL)
+	j = 0;
+	while (data->envp[j] != NULL)
 	{
-		if (i == i_oldpwd)
-			change_var_value_based_on_indexes(data, i, i_pwd);
-		else if (i == i_pwd)
-			change_pwd_value(data, i_homevar, i);
+		if (j == index.oldpwd)
+			change_var_value_based_on_indexes(j, index.pwd, data);
+		else if (j == index.pwd)
+			change_pwd_value(index, data); //cd_arg
 		else
 		{
-			data->new_envp[i] = ft_strdup(data->envp[i]);
-			if (data->new_envp[i] == NULL)
+			data->new_envp[j] = ft_strdup(data->envp[j]);
+			if (data->new_envp[j] == NULL)
 				exit(1);
 		}
-		i++;
+		j++;
 	}
-	if (i_oldpwd < 0)
+	if (index.oldpwd < 0)
 	{
-		change_var_value_based_on_indexes(data, i, i_pwd);
-		i++;
+		change_var_value_based_on_indexes(j, index.pwd, data);
+		j++;
 	}
 	data->new_envp[data->num_var] = NULL;
 	return (data->new_envp);
 }
 
-char**	cd_builtin(t_env_struct *data)
+char	*pick_argument_for_cd(char **arguments, const char *home_var)
 {
-	int i_homevar;
-	int i_pwd;
-	int i_oldpwd;
-	int len;
-	int fd;
+	char *path;
 
-	fd = data->fd_chosen;
-	i_pwd = number_var_in_list(data->envp, "PWD=") - 1;
-	i_oldpwd = number_var_in_list(data->envp, "OLDPWD=") - 1;
-	i_homevar = number_var_in_list(data->envp, "HOME=") - 1;
-	if (go_to_other_folder(data->arguments[0], data->envp[i_homevar], fd) >= 0)
+	path = NULL;
+	if (arguments != NULL)
+		path = ft_strdup(arguments[0]);
+	else
 	{
-		len = data->num_var + 1;
-		if (i_oldpwd < 0)
-			len++;
-		data->new_envp = malloc(sizeof(char *) * len); ///create new_envp, cope from data->envp if envp NULL, which I need to test
-		if (data->new_envp == NULL)
-			exit(1);
-		data->new_envp = fill_nulls(0, data->num_var, data->new_envp);
-		handle_enviromntl(data, i_homevar, i_pwd, i_oldpwd);
-		return (data->new_envp);
+		if (home_var != NULL)
+		{
+			while (*home_var != '=')
+				home_var++;
+			home_var++;
+			path = ft_strdup(home_var);
+		}
 	}
-	return (data->envp);
+	return (path);
 }
 
-//error messages \
-bash: cd: Docume/: No such file or directory \
+char	**change_enviromental_variable_after_cd(t_env_indexes i, t_env_struct *data)
+{
+	int len;
+	
+	i.pwd = number_var_in_list(data->envp, "PWD=") - 1;
+	i.oldpwd = number_var_in_list(data->envp, "OLDPWD=") - 1;
+	len = data->num_var + 1;
+	if (i.oldpwd < 0)
+		len++;
+	data->new_envp = malloc(sizeof(char *) * len); ///create new_envp, cope from data->envp if envp NULL, which I need to test
+	if (data->new_envp == NULL)
+		exit(1);
+	data->new_envp = fill_nulls(0, data->num_var, data->new_envp);
+	handle_enviromntl(i, data);
+	return (data->new_envp);
+}
+
+char	**cd_builtin(t_env_struct *data)
+{
+	t_env_indexes index;
+	char *cd_arg;
+
+	index.homevar = number_var_in_list(data->envp, "HOME=") - 1;
+	if (data->arguments == NULL && index.homevar < 0) ///bone CD command use HOME env var to go there but if unset HOME and not give any arg to cd there is no path
+	{
+		write_str_fd("minishell: cd: HOME not set", data->fd_chosen);
+		return (NULL);
+	}
+	cd_arg = pick_argument_for_cd(data->arguments, data->envp[index.homevar]);
+	if (go_to_other_folder(cd_arg, data->fd_chosen) >= 0)
+	{
+		data->new_envp = change_enviromental_variable_after_cd(index, data);
+		free(cd_arg);
+		return (data->new_envp);
+	}
+	free(cd_arg);
+	
+	
+	return (NULL);
+}
